f9903187e478ec4f7d8f19df8a6cb89fb9427cd6
Sudarshan K J <kjsudi@gmail.com>
2021-01-21 21:14:27 -0800

    Added init files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import Button from "react-bootstrap/Button";
+import "./App.css";
+import Accordion from "react-bootstrap/Accordion";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button variant="sudi">Primary</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
